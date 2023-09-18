3191c4eb1837aede0947109a3952bce7b187bb7e
cooperyen <dknick081@gmail.com>
2023-09-19 01:11:33 +0800

    0919

--- assets/socket.js
+++ assets/socket.js
@@ -24,7 +24,7 @@ const testURL = 'http://198.211.33.236:88'
 // const testURL = 'http://127.0.0.1:5000/'
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : testURL;
+const URL = testURL;
 
 export const socket = io(URL);
 
